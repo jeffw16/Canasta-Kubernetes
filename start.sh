002e3d6fdcea58616132af1d5413eaf764f8b9e7
Jeffrey Wang <jeffw16@users.noreply.github.com>
2025-06-21 17:38:01 -0700

    Ensure hostPath volumes auto-create dirs

--- start.sh
+++ start.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
 
 # Delete previous ConfigMaps
-kubectl delete configmap canasta-settings
-kubectl delete configmap canasta-config
-kubectl delete configmap canasta-env
+kubectl delete configmap canasta-settings 2>/dev/null
+kubectl delete configmap canasta-config 2>/dev/null
+kubectl delete configmap canasta-env 2>/dev/null
 
 # Create ConfigMaps from the configuration files
 kubectl create configmap canasta-settings --from-file=./settings/
 kubectl create configmap canasta-config --from-file=./config/
+
+# Ensure host directories exist for PersistentVolumes
+mkdir -p /canasta/persistent/caddy-data
+mkdir -p /canasta/persistent/mysql-data
+mkdir -p /canasta/persistent/sitemap-data
+mkdir -p /canasta/persistent/varnish-data
+mkdir -p /canasta/config-data/_initdb
+
 kubectl create configmap canasta-env --from-env-file=.env
 
 # Create all the Kubernetes deployments, services and related objects
